ae0450fe8e5ea5d8c4e7aab4702bae21d2f44801
tema <tema@tema-HP-ProBook-4510s.(none)>
2014-02-11 20:38:32 +0400

    *	Init commit for BKeeper

---
+++ backend/www/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * index.php
+ *
+ * @author: artuom proskunin <artuomv.proskunin@gmail.com>
+ * Date: 13.03.13
+ * Time: 15:43
+ */
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
+chdir(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..');
+
+require_once('common' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Yii' . DIRECTORY_SEPARATOR . 'yii.php');
+$config = require('backend' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'main.php');
+
+$app = Yii::createWebApplication($config);
+
+/**
+ * Тут можно зарегистрировать дополнительный Autoloader
+ * Например при использовании ZF library
+ *
+ * Yii::import('common.extensions.EZendAutoloader', true);
+ * EZendAutoloader::$prefixes = array('Zend');
+ * EZendAutoloader::$basePath = Yii::getPathOfAlias('common.lib') . DIRECTORY_SEPARATOR;
+ *
+ * Yii::registerAutoloader(array("EZendAutoloader", "loadClass"), true);
+ */
+
+$app->run();
---
+++ deploy/templates/frontend-main-devel-template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * main-devel.php
+ *
+ * @author: artuom proskunin <artuomv.proskunin@gmail.com>
+ */
+
+error_reporting(-1);
+ini_set('display_errors', true);
+
+$main_devel = array(
+
+);
+
+return $main_devel;
